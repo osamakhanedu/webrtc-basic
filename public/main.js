55708c719d42f42570c1bedd78f2e3ba7aa15419
Osama Khan <osama.khan@tabsera.com>
2022-02-10 11:27:37 +0500

    getting user media

--- public/main.js
+++ public/main.js
@@ -5,15 +5,35 @@ const goBtnEle = document.getElementById("goRoom");
 const localVideoEle = document.getElementById("localVideo");
 const remoteVideoEle = document.getElementById("remoteVideo");
 
-let roomName, localStream,remoteStream,rtcPeerConnection,isCaller;
+let roomName, localStream, remoteStream, rtcPeerConnection, isCaller;
 
 const iceServer = {
-    "iceServer": [
-        {"urls": "stun.l.google.com:19302"}
-    ]
-}
+  iceServer: [{ urls: "stun.l.google.com:19302" }],
+};
 
 const streamConstrains = {
-    audio: true,
-    video: true,
-}
+  audio: true,
+  video: true,
+};
+
+// get user media
+
+goBtnEle.onclick = () => {
+  if (roomNameEle.value === "") {
+    alert("Enter room name");
+  } else {
+    // getting user media
+    navigator.mediaDevices
+      .getUserMedia(streamConstrains)
+      .then((stream) => {
+        localStream = stream;
+        localVideoEle.srcObject = stream;
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+
+    selectRoomEle.style = "display:none";
+    meetingRoomEle.style = "display:block";
+  }
+};
